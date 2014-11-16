2e7dac840d93710304b8474ca6b97f7739cef7ea
ct-analytics <chrismtster@gmail.com>
2014-11-16 14:02:52 -0500

    added triples
    updated to a different example using triples

--- dplyr.R
+++ dplyr.R
@@ -1,35 +1,23 @@
 library("dplyr")
 library("Lahman")
 
-data(Salaries)
 data(Batting)
 data(Fielding)
 data(Master)
 
-head(Batting)
-
 Batting %>%
   group_by(playerID,yearID) %>%
   mutate(G_fielding = G - G_batting) %>%
-  summarize(G = sum(G),
-            G.fielding = sum(G_fielding),
-            G.batting = sum(G_batting),
-            team=paste(teamID,collapse=',')) %>%
+  summarize(triples = sum(X3B),
+            team=paste(teamID,collapse=','),
+            leagues.count=n_distinct(lgID)) %>%
   ungroup() %>%
-  arrange(desc(G.fielding)) %>%
-  filter(#yearID==2010,
-         G.fielding > 0) %>%
-  left_join(Fielding %>%
-              group_by(playerID,yearID) %>%
-              summarize(positions=paste(unique(POS),collapse=',')) %>%
-              ungroup() %>%
-              select(playerID,yearID,positions)) %>%
+  arrange(desc(triples)) %>%
+  filter(yearID==2004) %>%
   left_join(Master %>%
               mutate(name=paste(Master$nameFirst, Master$nameLast, sep=' ')) %>%
               select(playerID,name)) %>%
-  select(Name=name,Team=team,Positions=positions,Year=yearID,Games=G,
-         Games.Fielding.only=G.fielding,Games.Fielding.and.Batting=G.batting) %>%
-  filter(!("P" == Positions)) %>%
-  slice(1:10) 
+  select(Name=name,Team=team,Year=yearID,Triples=triples) %>%
+  slice(1:5) 
 
 
