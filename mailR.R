27603400d70d133fddb8a3fef866c85e879d2109
ct-analytics <chrismtster@gmail.com>
2015-10-04 08:21:44 -0400

    added in subject for email

--- mailR.R
+++ mailR.R
@@ -2,6 +2,7 @@ library(mailR)
 
 send.mail(from="feasible.analytics@gmail.com",
           to="feasible.analytics@gmail.com",
+          subect="test email from R",
           body="This is a test email to attach a file for review.",
           html=T,
           smtp=list(host.name = "smtp.gmail.com", 
